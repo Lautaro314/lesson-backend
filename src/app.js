43662e5da551e59f77cb666e160eabde329bec26
lautarofrioni <lauti.tatengue@hotmail.com>
2025-09-19 20:07:42 -0300

    Comenzando un nuevo proyecto Backend 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
+const { profile } = require("console");
 const express = require("express");
+const fs = require("fs")
 
 const app = express ();
 
 const PORT = 8080;
+
+
+
+
+app.get('/products', async (req,res) => {
+
+    try {
+    
+    const producto = await fs.promises.readFile('src/productos.json' , "utf-8");
+    
+
+    const productos = JSON.parse(producto)
+
+    res.json(productos)
+
+    } catch (error) {
+        console.error("ERROR AL CARGAR EL SERVIDOR" , error);
+    }  
+
+})
 
 
 app.listen(PORT , () => {
